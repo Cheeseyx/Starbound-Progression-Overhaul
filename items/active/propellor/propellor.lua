001c359724e1f26c4ad42335d549c013b3a05de2
Tara <cheeseyx@gmail.com>
2016-08-26 02:25:08 -0400

    Added propeller rotation/lighting
    Added lighting to the propeller and made it properly rotate to face the
    cursor

--- items/active/propellor/propellor.lua
+++ items/active/propellor/propellor.lua
@@ -1,13 +1,27 @@
 function init()
-	sb.logWarn("[debug] Init propellor")
-	self.swimDown=false
+	--sb.logWarn("[debug] Init propellor")
+	--self.swimDown=false
+	set()
 end
 
 
 function update(dt, fireMode, shiftHeld, moves)
-	status.addEphemeralEffect("fastSwim", 1)
+	status.addEphemeralEffect("fastSwim", 0.25)
 
-	self.last=swimDown
+
+  if self.fireMode == "alt" and self.lastFireMode ~= "alt" then
+    self.active = not self.active
+    sb.logWarn("[debug] Light toggle")
+    animator.setLightActive("flashlight", self.active)
+    animator.setLightActive("flashlightSpread", self.active)
+    animator.playSound("flashlight")
+  end
+  self.lastFireMode = fireMode
+
+  	self.aimAngle, self.facingDirection = activeItem.aimAngleAndDirection(0, activeItem.ownerAimPosition())
+  	activeItem.setFacingDirection(self.facingDirection)
+    activeItem.setArmAngle(self.aimAngle)
+	--self.last=swimDown
 	if mcontroller.liquidMovement() then
 		if moves.down then
 			--sb.logWarn("[debug] down")
@@ -20,4 +34,20 @@ function update(dt, fireMode, shiftHeld, moves)
 		end
 	end
 
+end
+
+function set()
+  animator.setLightActive("flashlight", true)
+  animator.setLightActive("flashlightSpread", true)
+  self.active = true
+end
+
+function reset()
+  animator.setLightActive("flashlight", false)
+  animator.setLightActive("flashlightSpread", false)
+  self.active = false
+end
+
+function uninit()
+  reset()
 end
